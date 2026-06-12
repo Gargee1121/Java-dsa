cd3797d5249d21cfe29abb2d419b500112acd29d
Gargee11 <gargeekanwa1121@gmail.com>
2026-06-12 08:57:55 +0530

    Added Two Sum solution using HashMap
    Implement TwoSum algorithm to find pairs that sum to a target.

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target sum: ");
+        int target = sc.nextInt();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        boolean found = false;
+
+        for(int i = 0; i < n; i++) {
+            int complement = target - arr[i];
+
+            if(map.containsKey(complement)) {
+                System.out.println("Pair Found: " + complement + " " + arr[i]);
+                found = true;
+                break;
+            }
+
+            map.put(arr[i], i);
+        }
+
+        if(!found) {
+            System.out.println("No Pair Found");
+        }
+
+        sc.close();
+    }
+}
