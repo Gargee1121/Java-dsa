2b6bd526f14152098f05e14cef04ebe062ec87bb
Gargee11 <gargeekanwa1121@gmail.com>
2026-06-17 18:24:33 +0530

    Added ValidPalindromeIgnoringSpecialChars class

---
+++ Strings/ValidPalindromeIgnoringSpecialChars.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class ValidPalindromeIgnoringSpecialChars {
+
+    public static boolean isPalindrome(String str) {
+
+        str = str.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter String: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("Valid Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+
+        sc.close();
+    }
+}
