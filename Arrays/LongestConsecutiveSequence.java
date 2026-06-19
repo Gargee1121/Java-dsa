9aa476212e9cf4b13f321bfb64d97c204c22b2b5
Gargee11 <gargeekanwa1121@gmail.com>
2026-06-19 09:07:50 +0530

    Added Longest Consecutive Sequence using HashSet

---
+++ Arrays/LongestConsecutiveSequence.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+
+public class LongestConsecutiveSequence {
+
+    public static void main(String[] args) {
+
+        int[] nums = {100, 4, 200, 1, 3, 2};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : set) {
+
+            if (!set.contains(num - 1)) {
+
+                int currentNum = num;
+                int count = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    count++;
+                }
+
+                longest = Math.max(longest, count);
+            }
+        }
+
+        System.out.println("Longest Consecutive Sequence Length: " + longest);
+    }
+}
