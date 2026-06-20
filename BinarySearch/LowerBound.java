f477f2bf7e31ba5fc7b35921470b28714fba1140
Gargee11 <gargeekanwa1121@gmail.com>
2026-06-20 09:24:13 +0530

    Added lower bound binary search in LowerBound.java

---
+++ BinarySearch/LowerBound.java
@@ -0,0 +1,32 @@
+public class LowerBound {
+
+    public static int lowerBound(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = arr.length;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 4, 4, 4, 6, 8};
+        int target = 4;
+
+        System.out.println("Lower Bound Index: "
+                + lowerBound(arr, target));
+    }
+}
