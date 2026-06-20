37cdf52ffdc3ac70aaaa7e7e4c9ffee72ae26576
Gargee11 <gargeekanwa1121@gmail.com>
2026-06-20 09:22:58 +0530

    Added binary search algorithm in Java

---
+++ BinarySearch/BinarySearch.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+
+    public static int binarySearch(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            if (arr[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+
+        System.out.print("Enter element to search: ");
+        int target = sc.nextInt();
+
+        int result = binarySearch(arr, target);
+
+        if (result != -1)
+            System.out.println("Element found at index " + result);
+        else
+            System.out.println("Element not found");
+
+        sc.close();
+    }
+}
