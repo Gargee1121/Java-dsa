c715f4a42c29d4b91f616e2753b8405a4f8c6659
Gargee11 <gargeekanwa1121@gmail.com>
2026-06-21 17:18:46 +0530

    Added Search Insert Position using Binary Search

---
+++ BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,32 @@
+public class SearchInsertPosition {
+
+    public static int searchInsert(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            if (arr[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return low;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 5, 6};
+        int target = 2;
+
+        System.out.println("Insert Position: "
+                + searchInsert(arr, target));
+    }
+}
