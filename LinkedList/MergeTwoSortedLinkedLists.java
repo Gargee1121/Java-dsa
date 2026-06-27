d451cf08c0b9d8848a96d9c5eb59b37a79abd2f4
Gargee11 <gargeekanwa1121@gmail.com>
2026-06-27 19:52:21 +0530

    Added Merge Two Sorted Linked Lists using Iterative Approach
    Implemented a method to merge two sorted linked lists into one sorted linked list. Added a print function to display the merged list.

---
+++ LinkedList/MergeTwoSortedLinkedLists.java
@@ -0,0 +1,60 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class MergeTwoSortedLinkedLists {
+
+    static Node merge(Node list1, Node list2) {
+
+        Node dummy = new Node(-1);
+        Node current = dummy;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.data <= list2.data) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+
+        if (list1 != null)
+            current.next = list1;
+        else
+            current.next = list2;
+
+        return dummy.next;
+    }
+
+    static void printList(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Node list1 = new Node(1);
+        list1.next = new Node(3);
+        list1.next.next = new Node(5);
+
+        Node list2 = new Node(2);
+        list2.next = new Node(4);
+        list2.next.next = new Node(6);
+
+        Node merged = merge(list1, list2);
+
+        printList(merged);
+    }
+}
