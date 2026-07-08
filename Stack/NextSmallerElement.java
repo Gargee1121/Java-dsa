57e407b9720afe71fb93795494b8612dec921b7c
Gargee11 <gargeekanwa1121@gmail.com>
2026-07-08 08:56:53 +0530

    Added NextSmallerElement class with nextSmaller method
    Implement a method to find the next smaller element for each element in the array using a stack.

---
+++ Stack/NextSmallerElement.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class NextSmallerElement {
+
+    public static int[] nextSmaller(int[] arr) {
+
+        int n = arr.length;
+        int[] ans = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!stack.isEmpty() && stack.peek() >= arr[i]) {
+                stack.pop();
+            }
+
+            ans[i] = stack.isEmpty() ? -1 : stack.peek();
+
+            stack.push(arr[i]);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {4, 8, 5, 2, 25};
+
+        int[] ans = nextSmaller(arr);
+
+        for (int x : ans)
+            System.out.print(x + " ");
+    }
+}
