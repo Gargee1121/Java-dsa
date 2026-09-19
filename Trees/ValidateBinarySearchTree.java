dc6e850b862b3df320777e269d9f6e9acfa1def6
Gargee11 <gargeekanwa1121@gmail.com>
2026-09-19 09:47:17 +0530

    Add ValidateBinarySearchTree class
    Implement a method to validate a binary search tree.

---
+++ Trees/ValidateBinarySearchTree.java
@@ -0,0 +1,54 @@
+public class ValidateBinarySearchTree {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static boolean isValidBST(TreeNode root) {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private static boolean validate(TreeNode node, long min, long max) {
+
+        if (node == null) {
+            return true;
+        }
+
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+
+        return validate(node.left, min, node.val)
+                && validate(node.right, node.val, max);
+    }
+
+    public static void main(String[] args) {
+
+        /*
+                 5
+                / \
+               3   7
+              / \ / \
+             2  4 6  8
+        */
+
+        TreeNode root = new TreeNode(5);
+
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(7);
+
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(8);
+
+        System.out.println("Is Valid BST: " + isValidBST(root));
+    }
+}
